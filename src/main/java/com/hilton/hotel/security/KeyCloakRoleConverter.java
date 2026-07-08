4ee9bb3a253442e2c43b90372a2ef5bcca09c14c
vishwageethanjana <geeth.me67@gmail.com>
2026-07-08 15:34:26 +0530

    scrum 25 | Keycloak implementation

---
+++ src/main/java/com/hilton/hotel/security/KeyCloakRoleConverter.java
@@ -0,0 +1,22 @@
+package com.hilton.hotel.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class KeyCloakRoleConverter implements Converter<Jwt , Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        Map<String, Object> realmAccess =
+                (Map<String, Object>) source.getClaims().get("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return List.of();
+        }
+        List<String> roles = (List<String>) realmAccess.get("roles");
+    }
+}
